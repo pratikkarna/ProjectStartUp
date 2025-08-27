73a1b29378e9a79cf1586cab21c0b37cff159767
AvishekKhatiwada <Khatiwadaavishek12@gmail.com>
2025-08-27 21:32:40 +0545

    Commit After template setup

---
+++ ProjectStartUp/Controllers/MasterController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectStartUp.Controllers
+{
+    public class MasterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        #region Account Group
+        public IActionResult AccountGroupList()
+        {
+            return View();
+        }
+        #endregion
+        #region AccountSubGroup
+        public IActionResult AccountSubGroupList()
+        {
+            return View();
+        }
+        #endregion
+        #region Agent
+        public IActionResult AgentList()
+        {
+            return View();
+        }
+        #endregion
+        #region Area
+        public IActionResult AreaList()
+        {
+            return View();
+        }
+        #endregion
+        #region Ledger
+        public IActionResult LedgerList()
+        {
+            return View();
+        }
+        #endregion
+    }
+}
